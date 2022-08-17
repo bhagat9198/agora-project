c8ffe82d86e6cc63e1bb96cbf316aee77d9a5319
bhagat singh <bhagatsinghsinghsingh1998@gmail.com>
2022-08-17 08:18:30 +0530

    streaming from host working

--- components/videopanel/videoPanelHost.js
+++ components/videopanel/videoPanelHost.js
@@ -20,7 +20,7 @@ export default function VideoPanelHost() {
     // Set the channel name.
     channel: "test",
     // Pass your temp token here.
-    token: "Your temp token",
+    token: "006cb587ba5fd94498e8b03260bbdec69c4IABmjlvsaWlUvUxVfUel6V1F/137//dIx8hn+/8E2e1sHgx+f9gAAAAAEAASvOWQNR39YgEAAQA9Hf1i",
     // Set the user ID.
     uid: 123456,
   };
@@ -30,54 +30,87 @@ export default function VideoPanelHost() {
     startBasicCall();
   }
 
-  
+
   async function startBasicCall() {
     // Create an AgoraRTCClient object.
     rtc.client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
 
+
+    //
+    // Join an RTC channel.
+    await rtc.client.join(options.appId, options.channel, options.token, options.uid);
+    // Create a local audio track from the audio sampled by a microphone.
+    rtc.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
+    // Create a local video track from the video captured by a camera.
+    rtc.localVideoTrack = await AgoraRTC.createCameraVideoTrack();
+    // Publish the local audio and video tracks to the RTC channel.
+    await rtc.client.publish([rtc.localAudioTrack, rtc.localVideoTrack]);
+    // Dynamically create a container in the form of a DIV element for playing the local video track.
+    const localPlayerContainer = document.createElement("div");
+    // Specify the ID of the DIV container. You can use the uid of the local user.
+    localPlayerContainer.id = options.uid;
+    localPlayerContainer.textContent = "Local user " + options.uid;
+    localPlayerContainer.style.width = "640px";
+    localPlayerContainer.style.height = "480px";
+    document.querySelector('#zzz').append(localPlayerContainer);
+    // document.body.append(localPlayerContainer);
+
+    // Play the local video track.
+    // Pass the DIV container and the SDK dynamically creates a player in the container for playing the local video track.
+    rtc.localVideoTrack.play(localPlayerContainer);
+    console.log("publish success!");
+
+
+
+
+
+
+
+
+
     // Listen for the "user-published" event, from which you can get an AgoraRTCRemoteUser object.
-    rtc.client.on("user-published", async (user, mediaType) => {
-      debugger
-      console.log('VideoPanel :: startBasicCall :: user-published :: user, mediaType :: ', user, mediaType);
-      // Subscribe to the remote user when the SDK triggers the "user-published" event
-      await rtc.client.subscribe(user, mediaType);
-      console.log("subscribe success");
+    // rtc.client.on("user-published", async (user, mediaType) => {
+    //   debugger
+    //   console.log('VideoPanel :: startBasicCall :: user-published :: user, mediaType :: ', user, mediaType);
+    //   // Subscribe to the remote user when the SDK triggers the "user-published" event
+    //   await rtc.client.subscribe(user, mediaType);
+    //   console.log("subscribe success");
 
-      // If the remote user publishes a video track.
-      if (mediaType === "video") {
-        // Get the RemoteVideoTrack object in the AgoraRTCRemoteUser object.
-        const remoteVideoTrack = user.videoTrack;
-        // Dynamically create a container in the form of a DIV element for playing the remote video track.
-        const remotePlayerContainer = document.createElement("div");
-        // Specify the ID of the DIV container. You can use the uid of the remote user.
-        remotePlayerContainer.id = user.uid.toString();
-        remotePlayerContainer.textContent = "Remote user " + user.uid.toString();
-        remotePlayerContainer.style.width = "640px";
-        remotePlayerContainer.style.height = "480px";
-        // document.body.append(remotePlayerContainer);
-        document.querySelector('#zzz').append(localPlayerContainer);
+    //   // If the remote user publishes a video track.
+    //   if (mediaType === "video") {
+    //     // Get the RemoteVideoTrack object in the AgoraRTCRemoteUser object.
+    //     const remoteVideoTrack = user.videoTrack;
+    //     // Dynamically create a container in the form of a DIV element for playing the remote video track.
+    //     const remotePlayerContainer = document.createElement("div");
+    //     // Specify the ID of the DIV container. You can use the uid of the remote user.
+    //     remotePlayerContainer.id = user.uid.toString();
+    //     remotePlayerContainer.textContent = "Remote user " + user.uid.toString();
+    //     remotePlayerContainer.style.width = "640px";
+    //     remotePlayerContainer.style.height = "480px";
+    //     // document.body.append(remotePlayerContainer);
+    //     document.querySelector('#zzz').append(localPlayerContainer);
 
-        // Play the remote video track.
-        // Pass the DIV container and the SDK dynamically creates a player in the container for playing the remote video track.
-        remoteVideoTrack.play(remotePlayerContainer);
-      }
+    //     // Play the remote video track.
+    //     // Pass the DIV container and the SDK dynamically creates a player in the container for playing the remote video track.
+    //     remoteVideoTrack.play(remotePlayerContainer);
+    //   }
 
-      // If the remote user publishes an audio track.
-      if (mediaType === "audio") {
-        // Get the RemoteAudioTrack object in the AgoraRTCRemoteUser object.
-        const remoteAudioTrack = user.audioTrack;
-        // Play the remote audio track. No need to pass any DOM element.
-        remoteAudioTrack.play();
-      }
+    //   // If the remote user publishes an audio track.
+    //   if (mediaType === "audio") {
+    //     // Get the RemoteAudioTrack object in the AgoraRTCRemoteUser object.
+    //     const remoteAudioTrack = user.audioTrack;
+    //     // Play the remote audio track. No need to pass any DOM element.
+    //     remoteAudioTrack.play();
+    //   }
 
-      // Listen for the "user-unpublished" event
-      rtc.client.on("user-unpublished", user => {
-        // Get the dynamically created DIV container.
-        const remotePlayerContainer = document.getElementById(user.uid);
-        // Destroy the container.
-        remotePlayerContainer.remove();
-      });
-    });
+    //   // Listen for the "user-unpublished" event
+    //   rtc.client.on("user-unpublished", user => {
+    //     // Get the dynamically created DIV container.
+    //     const remotePlayerContainer = document.getElementById(user.uid);
+    //     // Destroy the container.
+    //     remotePlayerContainer.remove();
+    //   });
+    // });
 
     // window.onload = function () {
     //   document.getElementById("join").onclick = async function () {
